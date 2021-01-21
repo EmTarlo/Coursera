cd6029d305e240b3b92ba3eec0ef609d4046527d
Francesco <francescoburchielli.fb@gmail.com>
2021-01-21 14:59:29 +0100

    Completed implementation of merge-sort algorithm

--- Algorithms&DataStructures/Week1/src/com/Main.java
+++ Algorithms&DataStructures/Week1/src/com/Main.java
@@ -6,26 +6,20 @@ public class Main {
 
 
     public static void main(String[] args) {
-        /*Logger logger = Logger.getLogger();
 
-        Karatsuba m = new Karatsuba();
+        com.MergeSort ms = new com.MergeSort();
 
-        BigDecimal result = m.kmultBigDecimal("434343","4545454");
-        //BigDecimal result = m.kmultBigDecimal("3141592653589793238462643383279502884197169399375105820974944592","2718281828459045235360287471352662497757247093699959574966967627");
-        System.out.println("\n"+result);*/
-        MergeSort ms = new MergeSort();
-
-        int[] array1 = {1,20,31,99};
-        int[] array2 = {2,10,45,82};
-        int[] out_array = ms.merge(array1,array2);
-
-        int size = out_array.length;
+        int[] array = {7,4,5,1,2,6,3};
         int m = 0;
+        int size = array.length;
+        int[] out_array = new int[size];
+        out_array = ms.mergeSort(array);
 
         while(m<=(size-1)){
             int a = out_array[m++];
             System.out.println("\n"+a);
         }
+
 
     }
 }
--- Algorithms&DataStructures/Week1/src/com/MergeSort.java
+++ Algorithms&DataStructures/Week1/src/com/MergeSort.java
@@ -1,15 +1,43 @@
 package com;
 
+
+import java.util.Arrays;
+
 public class MergeSort {
 
+    /* The following method is the core implementation of the mergeSort algorithm. It includes:
+            1) a recursive call for left half of the input array
+            2) recursive call for right half of the input array
+            3) call of the merge method defined further below
+    */
+    public int[] mergeSort(int[] array){
+        int middle = (array.length/2)-1;
+        int low = 0;
+        int high = array.length-1;
+        int [] left_array = Arrays.copyOfRange(array, low, middle+1);
+        int []right_array = Arrays.copyOfRange(array, middle+1, high + 1);
+
+        if(low<high){
+            left_array = mergeSort(left_array);
+            right_array = mergeSort(right_array);
+        }
+
+        int[] output = merge(left_array,right_array);
+        return output;
+
+    }
+
+
+    // The following method efficiently merges together two sorted arrays. The input arrays don't have to be of the same length.
     public int[] merge(int[] array1,int[] array2){
-        int org_size = array1.length;
-        int size = org_size*2;
+        int size_ar1 = array1.length;
+        int size_ar2 = array2.length;
+        int size = size_ar1+size_ar2;
         int[] out_array = new int[size];
         int i=0,j=0,k=0;
 
         while(k<=(size-1)){
-            while(i<=(org_size-1) && j<=(org_size-1)){
+            while(i<=(size_ar1-1) && j<=(size_ar2-1)){
                 if(array1[i]<array2[j]) {
                     out_array[k++] = array1[i++];
                 }else{
@@ -17,11 +45,11 @@ public class MergeSort {
                 }
             }
 
-            while(i<=(org_size-1)){
+            while(i<=(size_ar1-1)){
                 out_array[k++] = array1[i++];
             }
 
-            while(j<=(org_size-1)){
+            while(j<=(size_ar2-1)){
                 out_array[k++] = array2[j++];
             }
         }
