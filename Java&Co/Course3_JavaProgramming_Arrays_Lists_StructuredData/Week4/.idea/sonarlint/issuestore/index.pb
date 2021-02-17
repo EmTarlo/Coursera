561aefd4023acd7f4bf117c5e38a5951ba221fca
Francesco <francescoburchielli.fb@gmail.com>
2021-02-17 14:06:07 +0100

    Java Course: Vigenere Cypher/further work

---
+++ Java&Co/Course3_JavaProgramming_Arrays_Lists_StructuredData/Week4/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+P
+ src/com/Week4/CaesarCracker.java,d\5\d53eabbd7e5389cd614d8d92e1735e6789a308ea
+H
+src/com/Week4/Tests.java,a\e\ae5138aaed9a27d5560f5e3154d60e1f38f88d2f
+O
+src/com/Week4/CaesarCipher.java,2\c\2c3d345d1251d110d57c4f1f5e682707747c23b2
+G
+src/com/Week4/Main.java,d\e\de1d317bea979db255ab47bdde388719e1cf2030
+^
+.src/com/Week4/Input_Files/dictionaries/English,0\0\00ef06f0738aa922445257c3fe96795d86adb418
+Q
+!src/com/Week4/VigenereCipher.java,4\9\49e8009f5683efa80cd829a560533ddcece5da79
+R
+"src/com/Week4/VigenereBreaker.java,b\3\b3f548bafc8932201b5a6e10faddc1fa5f4414da
--- Java&Co/Course3_JavaProgramming_Arrays_Lists_StructuredData/Week4/src/com/Week4/VigenereBreaker.java
+++ Java&Co/Course3_JavaProgramming_Arrays_Lists_StructuredData/Week4/src/com/Week4/VigenereBreaker.java
@@ -1,7 +1,9 @@
 package com.Week4;
 
+import java.io.File;
 import java.util.*;
 import edu.duke.*;
+import org.jetbrains.annotations.NotNull;
 
 public class VigenereBreaker {
 
@@ -34,12 +36,55 @@ public class VigenereBreaker {
         return key;
     }
 
+
+    public HashSet<String> readDictionary(FileResource fr){
+        HashSet<String> dict = new HashSet<>();
+        for (String line:fr.lines()) {
+            String lw = line.toLowerCase();
+            dict.add(lw);
+        }
+        return dict;
+    }
+
+    public int countWords(@NotNull String message, HashSet<String> dictionary) {
+        int valid_words_count=0;
+        String[] split_msg = message.split("\\W+");
+        for (String word:split_msg) {
+            String low_case_word = word.toLowerCase();
+            if(dictionary.contains(low_case_word)){
+                valid_words_count++;
+            }
+        }
+        return valid_words_count;
+    }
+
+    public String breakForLanguage(String encrypted,HashSet<String> dictionary){
+        String decrypted = new String();
+        int how_many_real_words = 0;
+        int[] correct_key_set = null;
+
+        for(int i=1;i<=100;i++){
+            int[] curr_key_set = tryKeyLength(encrypted,i,'e');
+            VigenereCipher vc = new VigenereCipher(curr_key_set);
+            String curr_decrypted = vc.decrypt(encrypted);
+            int curr_count_real_words = countWords(curr_decrypted,dictionary);
+            if(curr_count_real_words>how_many_real_words){
+                how_many_real_words = curr_count_real_words;
+                decrypted = curr_decrypted;
+                correct_key_set = curr_key_set;
+            }
+        }
+        System.out.println("\nNumber of valid words found: "+how_many_real_words);
+        return decrypted;
+    }
+
+
     public void breakVigenere () {
         FileResource fr = new FileResource();
-        String file = fr.asString();
-        int[] key = tryKeyLength(file,4,'e');
-        VigenereCipher vc = new VigenereCipher(key);
-        String decrypted_msg = vc.decrypt(file);
+        String encrypted = fr.asString();
+        fr = new FileResource();
+        HashSet<String> dictionary = readDictionary(fr);
+        String decrypted_msg = breakForLanguage(encrypted,dictionary);
         System.out.println(decrypted_msg);
     }
     
